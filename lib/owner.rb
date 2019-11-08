4ef863e64cc9f9c47f0b0ea93ca5fcff4da666fe
Justin Phillips <justinphil02@yahoo.com>
2019-11-08 13:45:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+  @@all = []
+  def name
+    @name.dup.freeze
+  end
+  def species
+    @species.dup.freeze
+  end
+
+
+  def initialize(name)
+@name = name
+@species = "human"
+@@all << self
+  end
+
+def say_species
+"I am a #{species}."
 end
+
+### Class Methods ###
+
+def self.all
+@@all
+end
+
+def self.count
+  @@all.size
+end
+
+def self.reset_all
+  @@all.delete_if {true}
+end
+
+end #end of class
