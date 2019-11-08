ff24f7ce867c08ccfad88e450a9ac59007f480d6
Justin Phillips <justinphil02@yahoo.com>
2019-11-08 13:47:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,21 @@
 class Dog
-  attr_accessor :name
-  # code goes here
+  attr_accessor :owner, :mood
+ attr_reader :name
+@@all = []
+
+ def name
+@name.dup.freeze
+ end
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = 'nervous'
+    @@all << self
+  end
+
+  ###Class Methods ###
+
+  def self.all
+    @@all
+  end
 end
